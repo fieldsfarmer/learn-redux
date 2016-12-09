8b97b01641c2493eb25471ad3c607c671925f3e1
fieldsfarmer <fieldsfarmer@gmail.com>
2016-12-09 14:17:57 -0500

    add mapReducer and action generators

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('staring redux example');
 
@@ -93,22 +94,68 @@ var removeMovie = (id)=>{
   }
 }
 
+// Map reducer and action generators
+//---------------------------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action)=>{
+  switch(action.type){
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+  }
+}
+var startLocationFetch = ()=>{
+  return{
+    type: 'START_LOCATION_FETCH'
+  }
+}
+var completeLocationFetch  = (url)=>{
+  return{
+    type: 'COMPLETE_LOCATION_FETCH', url
+  }
+}
+
+var fetchLocation = ()=>{
+  store.dispatch(startLocationFetch());
+  axios.get('http://ipinfo.io').then(function(res){
+    let loc = res.data.loc;
+    let baseUrl = 'http://map.google.com?q=';
+    store.dispatch(completeLocationFetch(baseUrl+loc))
+  })
+}
+
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: moviesReducer
+  movies: moviesReducer,
+  map: mapReducer
 })
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension? window.devToolsExtension():f=>f));
 
-var currentState = store.getState();
-console.log('Current state', currentState);
-
 var unsubscribe = store.subscribe(()=>{
   var state = store.getState();
   console.log('State is', state);
-  document.getElementById('app').innerHTML = state.name;
+  if(state.map.isFetching){
+    document.getElementById('app').innerHTML = 'Loading';
+  } else if(state.map.url){
+    document.getElementById('app').innerHTML = `<a href = "${state.map.url}" target="_blank">View the map</a>`
+  }
 })
+
+
+var currentState = store.getState();
+console.log('Current state', currentState);
+
+fetchLocation();
 
 store.dispatch(changeName('Kevin'));
 store.dispatch(changeName('Lily'));
